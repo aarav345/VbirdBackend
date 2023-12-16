a21c935b5ea14bc8750a310530dc4958c84df1f7
aarav <aaravpradhan12365@gmail.com>
2023-12-16 16:45:30 +0545

    changes

--- config/database.py
+++ config/database.py
@@ -1,17 +1,21 @@
-from pymongo.mongo_client import MongoClient
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
+try:
+    # Connect to the MongoDB Atlas cluster
+    client = MongoClient("mongodb+srv://admin:Aaswon1%4034@cluster0.umdlv7g.mongodb.net/?retryWrites=true&w=majority")
 
+    # Access the 'bird_info' database and the 'bird_collection' collection
+    db = client.bird_info
+    collection_name = db["bird_collection"]
 
+    # Check if the connection is successful
+    if client.server_info():
+        print("Connected to MongoDB Atlas")
+    else:
+        print("Failed to connect to MongoDB Atlas")
 
-client = MongoClient("mongodb+srv://admin:Aaswon1%4034@cluster0.umdlv7g.mongodb.net/?retryWrites=true&w=majority")
-
-db = client.bird_info
-
-collection_name = db["bird_collection"]
-
-
-
-
-
-
-
+except ConnectionFailure as e:
+    print(f"Error connecting to MongoDB Atlas: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
