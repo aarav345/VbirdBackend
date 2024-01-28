2d9acec533ba084cdb5d2d6931b9190a5a172bdc
aarav <aaravpradhan12365@gmail.com>
2024-01-28 23:51:19 +0545

    added data of all 38 birds

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -2,10 +2,16 @@ def individual_serial(bird_info) -> dict:
     return {
         "id": str(bird_info["_id"]),
         "name": str(bird_info["name"]),
+        "length" : str(bird_info["length"]),
+        "wingspan": bird_info["wingspan"],
+        "mass" : bird_info["mass"],
         "scientificName": str(bird_info["scientificName"]),
         "description": str(bird_info["description"]),
         "location" : str(bird_info["location"]),
-        "imageUri": str(bird_info["imageUri"])
+        "audio" : bird_info["audio"],
+        "imageUri": str(bird_info["imageUri"]),
+        "video" : bird_info["video"]
+
     }
 
 
